566f09ff6985ea3935fecd3bc9894f5c378f771d
Vishal <vishal2004500@gmail.com>
2026-01-23 18:59:19 +0530

    bootstrap superuser for production

--- backend/store/apps.py
+++ backend/store/apps.py
@@ -1,6 +1,21 @@
 from django.apps import AppConfig
-
+from django.contrib.auth import get_user_model
+import os
 
 class StoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'store'
+
+    def ready(self):
+        User = get_user_model()
+
+        username = os.getenv("DJANGO_SUPERUSER_USERNAME", "admin")
+        email = os.getenv("DJANGO_SUPERUSER_EMAIL", "admin@example.com")
+        password = os.getenv("DJANGO_SUPERUSER_PASSWORD", "admin123")
+
+        if not User.objects.filter(username=username).exists():
+            User.objects.create_superuser(
+                username=username,
+                email=email,
+                password=password
+            )
